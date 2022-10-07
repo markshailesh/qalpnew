84f6117ad769ba87ca2110e2fe168da6660bdbd9
markshailesh <shaileshhq@gmail.com>
2022-10-07 11:45:05 +0530

    update button

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -64,11 +64,9 @@
                     </div>
 
                     <div class="form-group row mb-0">
-                        <div class="col-md-6 offset-md-4">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('Reset Password') }}
                             </button>
-                        </div>
                     </div>
                 </form>
             </div>
